da535ef152c3522a710eb2ded62fea9feb53598e
naluroncia <dasilvaanaluisa2116@gmail.com>
2023-10-01 20:44:57 -0300

    Atualização na pagina de pesquisa do usuário

--- lib/scr/pagina_pesquisa.dart
+++ lib/scr/pagina_pesquisa.dart
@@ -10,14 +10,15 @@ class PaginaPrincipal extends StatefulWidget {
 
 class PaginaPrincipalState extends State<PaginaPrincipal> {
   List<String> marca = ["Renault", "Fiat", "Toyota", "Ford", "Chevrolet", "Honda", "Hyundai", "Mitsubishi", "Volkswagen"];
-  List<String> ano = ["2000 - 2005", "2005 - 2010", "2010 - 2020", "< 2020"];
-  List<String>  modelo = ["modelo 1", "modelo 2", "modelo 3"];
-  List<String> km = ["0km - 10000km", "10000km - 20000km", "20000km - 30000km", "50000km - 60000km", "< 60000km"];
+  List<String> ano = ["2000 - 2005", "2005 - 2010", "2010 - 2020", "acima de 2020"];
+  List<String> modelo = ["modelo 1", "modelo 2", "modelo 3"];//terminar
+  List<String> km = ["0km - 10000km", "10000km - 20000km", "20000km - 30000km", "50000km - 60000km", "mais que 60000km"];
   List<String> carroceria = ["Buggy", "Conversível", "Cupê", "Hatchback", "Sedâ", "Minivan", "Perua/SW", "Picape", "Esportivo", "Van"];
   List<String> cor = ["Preto", "Prata", "Branco", "Vermelho", "Marrom", "Azul", "Amarelo", "Outra"];
   List<String> novoounao = ["novo", "usado"];
   List<String> preco = ["caro", "nao caro"];
   List<String> tabelaFIP = ["Comparar", "não comparar"];
+  List<String> loca = ["próximo a mim", "qualquer lugar"];
   List<String> relevancia = ["Mais Relevantes", "Maior Preço", "Menor Preço", "Menor KM"];
 
   final valorMarca = ValueNotifier('');
@@ -30,15 +31,18 @@ class PaginaPrincipalState extends State<PaginaPrincipal> {
   final valorPreco = ValueNotifier('');
   final valorTabelaFIP = ValueNotifier('');
   final valorRelev = ValueNotifier('');
+  final valorLoca = ValueNotifier('');
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
       drawer: NavBar(),
+
       //cabeçalho
       appBar: AppBar(
-        title: const Text('Olá!', style: TextStyle(fontSize: 18, color : Colors.white)),
+        title: const Text('Olá, <Nome>!', style: TextStyle(fontSize: 15, color : Colors.white)),
         actions: <Widget>[
           IconButton(
             onPressed: () {}, 
@@ -63,54 +67,75 @@ class PaginaPrincipalState extends State<PaginaPrincipal> {
         ),
         toolbarHeight: 110,
         iconTheme: const IconThemeData(color: Colors.white),
-        elevation: 0,
-        bottom:  PreferredSize(
-          preferredSize: const Size(500,60),
-          child: Column(
-            children: <Widget> [
-              Container(
-                height: 20,
-                color : Colors.white.withOpacity(0.5),
-              ),
-              Container(
-                height: 60,
-                color: Colors.white.withOpacity(0.5),
-                child : Padding(
-                  padding: const EdgeInsets.all(7),
-                  child: Container(
-                    height: 5,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadiusDirectional.circular(15),
-                      color : Colors.white
-                    ),
-                    child : TextFormField(
-                      decoration: const InputDecoration(
-                        prefixIcon: Icon(Icons.search),
-                        hintText: "Busque seu carro aqui!",
-                        hintStyle: TextStyle(fontSize:15)
-                      ),
-                    )
-                  )
-                )
-              ),
-              Container(
-                height: 20,
-                color : Colors.white.withOpacity(0.5),
-              )
-            ],
-          )
-        ),
       ),
-      body: Stack(
-        children: [
-         Container(
-              height: 180,
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(0.0),
+
+      body: Container(
+        color: const Color.fromARGB(255, 237, 235, 235),
+        child : Stack(
+          children: [
+            Positioned(
+              top: 100,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(25.0),
+                color: const Color.fromARGB(150, 135, 157, 168),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Busque seu carro aqui',
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15))
+                    )
+                  ),
+                )
+              ), 
+            ),
+            
+            Positioned(
+              top: 220,
+              left: 20,
+              right: 20,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5), // Define a cor da sombra
+                      blurRadius: 2, // Define o desfoque da sombra
+                      offset: const Offset(0,4), // Define o deslocamento da sombra
+                    ),
+                  ],
+                ),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    
+                    IconButton(
+                      onPressed: () {
+                        valorMarca.value = '';
+                        valorAno.value = '';
+                        valorModelo.value = '';
+                        valorKM.value = '';
+                        valorCarroceria.value = '';
+                        valorCor.value = '';
+                        valorNov.value = '';
+                        valorPreco.value = '';
+                        valorTabelaFIP.value = '';
+                        valorRelev.value = '';
+                        valorLoca.value = '';
+                      }, 
+                    icon: const Icon(Icons.loop_outlined)),
+
+                    //filtro marca
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
                         child: ValueListenableBuilder(
                           valueListenable: valorMarca, builder: (BuildContext constext, String value, _){
                             return DropdownButton<String>(
@@ -125,15 +150,199 @@ class PaginaPrincipalState extends State<PaginaPrincipal> {
                           })
                         
                       ),
-                    ],
-                  ),
 
-                ],
+                    //filtro Ano
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorAno, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Ano"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorAno.value = escolha.toString(),
+                              items: ano.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+                      
+                    //filtro Modelo
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorModelo, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Modelo"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorModelo.value = escolha.toString(),
+                              items: modelo.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                    //filtro KM
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorKM, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Quilometragem"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorKM.value = escolha.toString(),
+                              items: km.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                    //filtro Carroceria
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorCarroceria, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Carroceria"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorCarroceria.value = escolha.toString(),
+                              items: carroceria.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                    //filtro cor
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorCor, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Cor"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorCor.value = escolha.toString(),
+                              items: cor.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                      //filtro novo ou não
+                      Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorNov, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Usado"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorNov.value = escolha.toString(),
+                              items: novoounao.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                      //filtro preço
+                      Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorPreco, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Preço"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorPreco.value = escolha.toString(),
+                              items: preco.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                      //filtro tabelaFIP
+                      Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorTabelaFIP, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Tabela FIP"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorTabelaFIP.value = escolha.toString(),
+                              items: tabelaFIP.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                      //filtro localização
+                      Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorLoca, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Localização"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorLoca.value = escolha.toString(),
+                              items: loca.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+                      //filtro relevancia
+                      Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: ValueListenableBuilder(
+                          valueListenable: valorRelev, builder: (BuildContext constext, String value, _){
+                            return DropdownButton<String>(
+                              hint: const Text("Relevancia"),
+                              value: (value.isEmpty) ? null : value,
+                              onChanged: (escolha) => valorRelev.value = escolha.toString(),
+                              items: relevancia.map((opcao) => DropdownMenuItem(
+                                value: opcao,
+                                child: Text(opcao),
+                              )).toList(),
+                            );
+                          })
+                        
+                      ),
+
+
+
+                  ],
                 )
-            ),
-        ],
-      ),
-      
+                )
+                
+                                
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
